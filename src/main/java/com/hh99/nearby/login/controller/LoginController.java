63d80e4db6865ba83bd1a1b7224be2a3e3de3625
Oh-Myeongjae <omjcws@gmail.com>
2022-08-29 14:32:17 +0900

    feat(로그인): 로그인 초기 세팅

--- src/main/java/com/hh99/nearby/login/controller/LoginController.java
+++ src/main/java/com/hh99/nearby/login/controller/LoginController.java
@@ -1,4 +1,16 @@
 package com.hh99.nearby.login.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RestController
 public class LoginController {
+
+    @PostMapping("/api/login")
+    public ResponseEntity<?> login (){
+
+        return ResponseEntity.ok("");
+    }
 }
