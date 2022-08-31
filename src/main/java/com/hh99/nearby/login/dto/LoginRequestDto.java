292dae0a3a4d0016e949e40715cca4779f1cba05
Oh-Myeongjae <omjcws@gmail.com>
2022-08-31 14:31:05 +0900

    feat(로그인): 로그인 응답메시지 변경 #9

--- src/main/java/com/hh99/nearby/login/dto/LoginRequestDto.java
+++ src/main/java/com/hh99/nearby/login/dto/LoginRequestDto.java
@@ -1,4 +1,15 @@
 package com.hh99.nearby.login.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
 public class LoginRequestDto {
+
+    private String username;
+    private String password;
+
 }
--- src/main/java/com/hh99/nearby/login/service/LoginService.java
+++ src/main/java/com/hh99/nearby/login/service/LoginService.java
@@ -1,4 +1,64 @@
 package com.hh99.nearby.login.service;
 
+import com.hh99.nearby.ResponseDto;
+import com.hh99.nearby.entity.Member;
+import com.hh99.nearby.login.dto.LoginRequestDto;
+import com.hh99.nearby.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
 public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public ResponseDto<?> login(LoginRequestDto requestDto, HttpServletResponse response) {
+        Member member = isPresentMemberByUsername(requestDto.getUsername());
+        if (null == member) {
+            return ResponseDto.fail("400", "Not existing email or wrong password");
+        }
+
+        if (!member.validatePassword(passwordEncoder, requestDto.getPassword())) {
+            return ResponseDto.fail("400", "Not existing email or wrong password");
+        }
+
+//        TokenDto tokenDto = tokenProvider.generateTokenDto(member);
+//        tokenToHeaders(tokenDto, response);
+
+        return ResponseDto.success(null, "200", "Successfully logged in");
+    }
+
+    public ResponseDto<?> logout(HttpServletRequest request) {
+
+//        Member member = isPresentMemberByUsername(requestDto.getUsername());
+//        if (null == member) {
+//            return ResponseDto.fail("400",
+//                    "사용자를 찾을 수 없습니다.");
+//        }
+//        tokenProvider.deleteRefreshToken(member);
+//
+       return ResponseDto.success(null, "200", "Successfully logged out");
+    }
+
+    @Transactional(readOnly = true)
+    public Member isPresentMemberByUsername(String username) {
+        Optional<Member> optionalMember = memberRepository.findByUsername(username);
+        return optionalMember.orElse(null);
+    }
+
+//    public void tokenToHeaders(TokenDto tokenDto, HttpServletResponse response) {
+//        response.addHeader("Authorization", "Bearer " + tokenDto.getAccessToken());
+////        response.addHeader("RefreshToken", tokenDto.getRefreshToken());
+//        response.addHeader("Access-Token-Expire-Time", tokenDto.getAccessTokenExpiresIn().toString());
+//    }
 }
