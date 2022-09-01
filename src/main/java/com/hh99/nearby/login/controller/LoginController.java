a4c487eda5c46a3a974c4a39723f90dc7ea3ac3c
Oh-Myeongjae <omjcws@gmail.com>
2022-09-01 14:13:49 +0900

    feat(로그인,시큐리티): 로그인, 시큐리티 초기세팅 #9 #10

--- src/main/java/com/hh99/nearby/login/controller/LoginController.java
+++ src/main/java/com/hh99/nearby/login/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.hh99.nearby.login.controller;
 
 import com.hh99.nearby.login.dto.LoginRequestDto;
+import com.hh99.nearby.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,23 +12,25 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class LoginController {
 
+    private final LoginService loginService;
+
+
     @PostMapping("/api/login")
     public ResponseEntity<?> login (){
-
         return ResponseEntity.ok("");
     }
 
     //로그인
     @RequestMapping(value = "/api/login", method = RequestMethod.POST)
     public ResponseEntity<?> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
-//        return memberService.login(requestDto, response);
+        loginService.login(requestDto, response);
         return ResponseEntity.ok("");
     }
 
     //로그아웃
     @RequestMapping(value = "/api/logout", method = RequestMethod.POST)
     public ResponseEntity<?> logout(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
-//        return memberService.logout(request);
+         loginService.logout(requestDto);
         return ResponseEntity.ok("");
     }
 }
