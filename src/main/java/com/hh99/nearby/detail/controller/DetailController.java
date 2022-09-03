27087f2cf8d549add86bb0b841bfc3e3191d18c6
projectmiluju <dnjsdyd0712@naver.com>
2022-09-03 23:25:18 +0900

    fix(상세 모달): api url 변경 #33
    /api/post/{id} -> /api/challenge/{id}

--- src/main/java/com/hh99/nearby/detail/controller/DetailController.java
+++ src/main/java/com/hh99/nearby/detail/controller/DetailController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,7 +15,7 @@ public class DetailController {
     public final DetailService detailService;
 
     //상세 모달
-    @GetMapping("api/challenge/{id}")
+    @GetMapping("/api/challenge/{id}")
     public ResponseEntity<?> detailModal(@PathVariable Long id){
         return detailService.detailModal(id);
     }
